86b512183ef05e0a823778351f0bef2e9971e46b
Lloyd Maza <lloydpmaza@gmail.com>
2019-02-12 15:57:41 -0800

    Attempt to fix starling-toolchain tarball URL

--- starling-toolchain.rb
+++ starling-toolchain.rb
@@ -1,9 +1,9 @@
 class StarlingToolchain < Formula
   desc "Quick install of the Starling development toolchain"
   homepage "https://github.com/swift-nav/starling"
-  url "https://github.com/swift-nav/homebrew-swift-devs/blob/master/artifacts/starling-toolchain-v1.0.1.tar.gz"
+  url "https://github.com/swift-nav/homebrew-swift-devs/raw/master/artifacts/starling-toolchain-v1.0.1.tar.gz"
   sha256 "b144333564bf16771d01e33035466896b402cf8db005ebff0886f9ffdd1cffa9"
-  version "1.0.0"
+  version "1.0.1"
 
   bottle :unneeded
 

809b9b3617dc92168efd64cefa294fb960d07781
Lloyd Maza <lloydpmaza@gmail.com>
2019-02-12 15:49:50 -0800

    Bump starling-toolchain version in formula

--- starling-toolchain.rb
+++ starling-toolchain.rb
@@ -1,8 +1,8 @@
 class StarlingToolchain < Formula
   desc "Quick install of the Starling development toolchain"
   homepage "https://github.com/swift-nav/starling"
-  url "https://github.com/swift-nav/homebrew-swift-devs/blob/master/artifacts/starling-toolchain-v1.0.0.tar.gz"
-  sha256 "31bf63152617289e27c749775399489dca59800fadf13515bee166d9039a6c37"
+  url "https://github.com/swift-nav/homebrew-swift-devs/blob/master/artifacts/starling-toolchain-v1.0.1.tar.gz"
+  sha256 "b144333564bf16771d01e33035466896b402cf8db005ebff0886f9ffdd1cffa9"
   version "1.0.0"
 
   bottle :unneeded

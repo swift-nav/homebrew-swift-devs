aab456305f6e1508c0b05d9eae06d9e0114d3bdf
Lloyd Maza <lloydpmaza@gmail.com>
2019-03-20 10:54:37 -0700

    Clean up starling-toolchian, bump to v1.3.0

--- starling-toolchain.rb
+++ starling-toolchain.rb
@@ -3,35 +3,29 @@ class StarlingToolchain < Formula
   homepage "https://github.com/swift-nav/starling"
   url "http://releases.llvm.org/6.0.0/clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz"
   sha256 "0ef8e99e9c9b262a53ab8f2821e2391d041615dd3f3ff36fdf5370916b0f4268"
-  version "1.2.0"
+  version "1.3.0"
 
   bottle :unneeded
 
-  resource "clang-4.0" do
+  resource "llvm-4.0" do
     url "http://releases.llvm.org/4.0.0/clang+llvm-4.0.0-x86_64-apple-darwin.tar.xz"
     sha256 "5504064916d0651c185ceff85871298f31e2eaf4abf1333b2c36f5eed5e9cde6"
   end
 
-  resource "arm-gcc" do
-    url "https://developer.arm.com/-/media/Files/downloads/gnu-rm/6-2017q2/gcc-arm-none-eabi-6-2017-q2-update-mac.tar.bz2"
-    sha256 "7d3080514a2899d05fc55466cdc477e2448b6a62f536ffca3dd846822ff52900"
-  end
-
   def install
-    (buildpath/"clang-4.0").install resource("clang-4.0")
-    (buildpath/"gcc-arm-none-eabi-6").install resource("arm-gcc")
+    # Install the entire LLVM 6.0 toolchain
+    bin.install Dir["bin/*"]
+    include.install Dir["include/*"]
+    lib.install Dir["lib/*"]
+    libexec.install Dir["libexec/*"]
+    share.install Dir["share/*"]
 
-    rm "bin/clang-6.0"
-    mv "bin/clang", "bin/clang-6.0"
-    mv "bin/clang++", "bin/clang++-6.0"
-    mv buildpath/"clang-4.0/bin/clang-format", buildpath/"clang-4.0/bin/clang-format-4.0"
-    mv buildpath/"clang-4.0/bin/clang-tidy", buildpath/"clang-4.0/bin/clang-tidy-4.0"
-    mv buildpath/"gcc-arm-none-eabi-6/bin", buildpath/"gcc-arm-none-eabi-6/arm-gcc"
+    # Install clang-format and clang-tidy from LLVM 4.0 only
+    (buildpath/"llvm-4.0").install resource("llvm-4.0")
+    mv buildpath/"llvm-4.0/bin/clang-format", buildpath/"llvm-4.0/bin/clang-format-4.0"
+    mv buildpath/"llvm-4.0/bin/clang-tidy", buildpath/"llvm-4.0/bin/clang-tidy-4.0"
 
-    bin.install "bin/clang-6.0"
-    bin.install "bin/clang++-6.0"
-    bin.install buildpath/"clang-4.0/bin/clang-format-4.0"
-    bin.install buildpath/"clang-4.0/bin/clang-tidy-4.0"
-    bin.install Dir[buildpath/"gcc-arm-none-eabi-6/arm-gcc/*"]
+    bin.install buildpath/"llvm-4.0/bin/clang-format-4.0"
+    bin.install buildpath/"llvm-4.0/bin/clang-tidy-4.0"
   end
 end

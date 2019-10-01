07596348d37eac6f74c9636056c90e96dce8aa55
Joseph Angelo <joseph.angelo@swift-nav.com>
2019-10-01 11:32:12 -0700

    Added a dependency for installing the check library (#2)

---
+++ starling-clang-tools.rb
@@ -0,0 +1,14 @@
+class StarlingClangTools < Formula
+  desc "Installs the clang-format and clang-tidy versions used in starling"
+  homepage "https://github.com/swift-nav/starling"
+  url "file:///dev/null"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  version "1.0.0"
+  depends_on "llvm@6"
+
+  bottle :unneeded
+
+  def install
+    bin.install_symlink "/usr/local/opt/llvm@6/bin/clang-format" => "clang-format-6.0", "/usr/local/opt/llvm@6/bin/clang-tidy" => "clang-tidy-6.0"
+  end
+end
--- starling-toolchain.rb
+++ starling-toolchain.rb
@@ -1,22 +1,18 @@
 class StarlingToolchain < Formula
   desc "Quick install of the Starling development toolchain"
   homepage "https://github.com/swift-nav/starling"
-  url "http://releases.llvm.org/6.0.0/clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz"
-  sha256 "0ef8e99e9c9b262a53ab8f2821e2391d041615dd3f3ff36fdf5370916b0f4268"
-  version "1.4.0"
+  url "file:///dev/null"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  version "2.0.0"
+
+  depends_on "starling-clang-tools"
+  depends_on "check"
 
   bottle :unneeded
 
   def install
-    # Also install clang-format and clang-tidy with version names
-    cp "bin/clang-format", "bin/clang-format-6.0"
-    cp "bin/clang-tidy", "bin/clang-tidy-6.0"
-
-    # Install the entire LLVM 6.0 toolchain
-    bin.install Dir["bin/*"]
-    include.install Dir["include/*"]
-    lib.install Dir["lib/*"]
-    libexec.install Dir["libexec/*"]
-    share.install Dir["share/*"]
+    (doc/"README").write <<~EOS
+      Check out the Starling repository README for details on the starling project. https://github.com/swift-nav/starling
+    EOS
   end
 end

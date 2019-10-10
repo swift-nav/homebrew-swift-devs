675f516c6a8cd0814bd0ef93e2e2b53e75388a0e
Joseph Angelo <joseph.angelo@swift-nav.com>
2019-10-10 12:37:31 -0700

    Added a formula to install the piksi firmware toolchain (#3)

---
+++ piksi-firmware-toolchain.rb
@@ -0,0 +1,19 @@
+class PiksiFirmwareToolchain < Formula
+  desc "Installs the toolchain used to build the Piksi firmware"
+  homepage "https://github.com/swift-nav/starling"
+  url "https://github.com/swift-nav/swift-toolchains/releases/download/pfwp_mac_toolchain/gcc-arm-none-eabi-6-2017-q2-update-mac.tar.bz2"
+  sha256 "7d3080514a2899d05fc55466cdc477e2448b6a62f536ffca3dd846822ff52900"
+  version "1.0.0"
+
+  bottle :unneeded
+
+  conflicts_with "arm-none-eabi-gcc", :because => "piksi-firmware-tools also installs arm-none-eabi-gcc"
+
+  depends_on "cmake"
+
+  def install
+    # This incantation borrowed from the ARMmbed homebrew formula
+    (prefix/"gcc").install Dir["./*"]
+    Dir.glob(prefix/"gcc/bin/*") { |file| bin.install_symlink file }
+  end
+end

f3e9828c65e685d555c4bba6ebcc014e95e538d3
Christian Reimer <christian.reimer@swift-nav.com>
2020-09-24 12:40:01 +0200

    Coverage

--- starling-clang-tools.rb
+++ starling-clang-tools.rb
@@ -4,12 +4,15 @@ class StarlingClangTools < Formula
   url "file:///dev/null"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   version "1.0.0"
-  
+
   depends_on "llvm@6"
 
   bottle :unneeded
 
   def install
-    bin.install_symlink "/usr/local/opt/llvm@6/bin/clang-format" => "clang-format-6.0", "/usr/local/opt/llvm@6/bin/clang-tidy" => "clang-tidy-6.0"
+    bin.install_symlink "/usr/local/opt/llvm@6/bin/clang-format" => "clang-format-6.0", 
+                        "/usr/local/opt/llvm@6/bin/clang-tidy" => "clang-tidy-6.0",
+                        "/usr/local/opt/llvm@6/bin/llvm-cov" => "llvm-cov"
+    
   end
 end

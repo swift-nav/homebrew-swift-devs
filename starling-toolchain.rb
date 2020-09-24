6ae91ed1f91a9432c1f22d2cf8db235e73d3c6a0
Christian Reimer <christian.reimer@swift-nav.com>
2020-09-24 21:59:42 +0200

    Add more toolchain components (#4)

--- starling-toolchain.rb
+++ starling-toolchain.rb
@@ -3,10 +3,13 @@ class StarlingToolchain < Formula
   homepage "https://github.com/swift-nav/starling"
   url "file:///dev/null"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-  version "2.0.0"
+  version "3.0.0"
 
   depends_on "starling-clang-tools"
+  depends_on "starling-python-tools"
   depends_on "check"
+  depends_on "gh"
+  depends_on "cmake"
 
   bottle :unneeded
 

a8093b4d47eee9e4ae76a55abe35f8461aa1c06d
Christian Reimer <christian.reimer@swift-nav.com>
2020-12-08 15:55:17 +0100

    Remove old 'Brewfiles', add Cask for clang tools

---
+++ Casks/clang-tools-6.0.rb
@@ -0,0 +1,12 @@
+cask "clang-tools-6.0" do
+    version "6.0.0"
+    sha256 "0ef8e99e9c9b262a53ab8f2821e2391d041615dd3f3ff36fdf5370916b0f4268"
+  
+    url "https://releases.llvm.org/6.0.0/clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz"
+    name "clang-format"
+    desc "Clang formatting tool"
+    homepage "https://www.llvm.org/"
+  
+    binary "clang+llvm-6.0.0-x86_64-apple-darwin/bin/clang-format", target: "clang-format-6.0"
+    binary "clang+llvm-6.0.0-x86_64-apple-darwin/bin/clang-tidy", target: "clang-tidy-6.0"
+  end
